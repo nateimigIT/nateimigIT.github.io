c51ed930050bc7feba88655604cac04e2e026a72
Nathan Imig <imig.nathan@gmail.com>
2018-03-19 23:54:26 -0500

    ni

--- Color_Gradient/script.js
+++ Color_Gradient/script.js
@@ -4,7 +4,7 @@ var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 
-// set css styles
+// set css styles real good
 function setGredient(){
 	body.style.background = "linear-gradient(to right, " 
 	+ color1.value 
