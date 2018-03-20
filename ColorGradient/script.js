bd0f82d78e7be75f0106be4d38d8e719e14e4fc1
Nathan Imig <imig.nathan@gmail.com>
2018-03-19 23:44:03 -0500

    Color Gradient
    @nateimigit

---
+++ ColorGradient/script.js
@@ -0,0 +1,20 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+
+// set css styles
+function() setGredient{
+	body.style.background = "linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value
+	+ ")";
+}
+
+//do not call the functoin with the (), simply name the function
+//to call when event is triggered
+color1.addEventListener("input", setGredient);
+color2.addEventListener("input", setGredient);
+
